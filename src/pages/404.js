814b2fff0d70cdde63fdf2701fb9912f13efd4cd
Jaap de Haan <ColorOfCode@googlemail.com>
2019-01-15 00:24:15 +0100

    Closes #7 out put the picture on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import skull from '../images/wallpaper-skull.jpg';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404 - NOT FOUND</h1>
+    <img src={skull} alt="Skull" />
+    <div
+      style={{
+        float: 'right',
+      }}
+    >
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
   </Layout>
 );
 
