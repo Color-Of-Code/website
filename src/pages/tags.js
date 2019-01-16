8adffad9b74365f2e696b618ae3eac45821c8b59
Jaap de Haan <ColorOfCode@googlemail.com>
2019-01-17 00:49:16 +0100

    #10 Added searchable tag list

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -7,6 +7,45 @@ import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import Tag from '../components/tag';
 
+class SearchableTagList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allTags: props.tags,
+      displayedTags: props.tags,
+    };
+  }
+
+  searchHandler = event => {
+    let searchQuery = event.target.value.toLowerCase();
+    var filteredTags = this.state.allTags.filter(el => {
+      let searchValue = el.fieldValue.toLowerCase();
+      return searchValue.indexOf(searchQuery) !== -1;
+    });
+    this.setState({
+      displayedTags: filteredTags,
+    });
+  };
+
+  render() {
+    let tags = this.state.displayedTags;
+    return (
+      <div className="searchable-tag-list">
+        <div className="search">
+          <input type="text" className="search" onChange={this.searchHandler} />
+        </div>
+        <div className="tags">
+          {tags.map(tag => (
+            <span key={tag.fieldValue}>
+              <Tag tag={tag.fieldValue} count={tag.totalCount} />
+            </span>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
 const TagsPage = ({
   data: {
     allMarkdownRemark: { group },
@@ -18,17 +57,20 @@ const TagsPage = ({
   <Layout>
     <div className="tags">
       <Helmet title={title} />
-      <div>
-        <h1>Tags</h1>
-        {group.map(tag => (
-          <span key={tag.fieldValue}>
-            <Tag tag={tag.fieldValue} count={tag.totalCount} />
-          </span>
-        ))}
-      </div>
+      <h1>Tags</h1>
+      <SearchableTagList tags={group} />
     </div>
   </Layout>
 );
+
+SearchableTagList.propTypes = {
+  tags: PropTypes.arrayOf(
+    PropTypes.shape({
+      fieldValue: PropTypes.string.isRequired,
+      totalCount: PropTypes.number.isRequired,
+    }).isRequired
+  ),
+};
 
 TagsPage.propTypes = {
   data: PropTypes.shape({
