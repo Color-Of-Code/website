b90941038bed98aa72b06e0cb88ee18cb37ca255
Jaap de Haan <ColorOfCode@googlemail.com>
2019-01-26 17:23:12 +0100

    #8 Added URL exist check for testing purpose

--- plugins/gatsby-remark-embellish-links/index.js
+++ plugins/gatsby-remark-embellish-links/index.js
@@ -2,21 +2,28 @@
 
 const visit = require('unist-util-visit');
 const defaultSites = require('./sites');
+var urlExists = require('url-exists');
 
 module.exports = ({ markdownAST }, { sites } = {}) => {
   visit(markdownAST, 'link', node => {
     const linkMap = sites || defaultSites;
 
-    Object.keys(linkMap).forEach(icon => {
+    for (var icon in linkMap) {
       if (node.url.match(linkMap[icon])) {
         node.children.unshift({
           type: 'html',
           value: `<i class="${icon}"></i>`,
         });
+        // TODO: allow this check via a npm task somehow
+        urlExists(node.url, function(err, exists) {
+          if (!exists) {
+            console.log('URL ERROR: ' + node.url);
+          }
+        });
         // first hit wins
         return;
       }
-    });
+    }
   });
 };
 
